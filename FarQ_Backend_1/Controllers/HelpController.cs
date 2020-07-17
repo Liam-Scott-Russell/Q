cc2501a964733b9aecf09a37949e8c2ddb9f31eb
Liam Scott-Russell <liam.scott.russell@gmail.com>
2020-07-18 03:57:41 +1200

    Add get request for help link

---
+++ FarQ_Backend_1/Controllers/HelpController.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FarQ_Backend_1.Context;
+using Q_Backend.Models;
+
+namespace FarQ_Backend_1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelpController : ControllerBase
+    {
+        private readonly FarQContext _context;
+
+        public HelpController(FarQContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet("{eventID}")]
+        public async Task<ActionResult<Event>> GetEvent(int eventID)
+        {
+            var events = from e in _context.Event select e;
+            var helpLink = events.First(e => e.EventID.Equals(eventID))?.HelpLink;
+
+            if (helpLink == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(helpLink);
+        }
+    }
+}
